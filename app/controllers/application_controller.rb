687df79a45a0f329f1297397d4af589b34b4e9c4
Andres <andreslemik@yandex.ru>
2015-07-21 18:03:35 +0300

    active_admin authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def access_denied(exception)
+    redirect_to root_path, alert: exception.message
+    Rails.logger.error("Redirected by #{caller(1).first rescue 'unknown'}")
+  end
 end
