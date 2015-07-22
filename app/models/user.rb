bbeca3f301ba0bfe85ac57a950e3ccdfee6d5fb6
Andres <andreslemik@yandex.ru>
2015-07-22 13:43:35 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
+    if login = conditions.delete(:login_key)
       where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
       where(conditions.to_h).first
