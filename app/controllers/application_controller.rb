4fa836b52ee34540d0d9f1ea80883c0871bfdb01
Andres <andreslemik@yandex.ru>
2015-07-27 21:27:25 +0300

    change login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
   before_action :configure_permitted_params, if: :devise_controller?
+  before_action :authenticate_user!
 
   def access_denied(exception)
     redirect_to root_path, alert: exception.message
