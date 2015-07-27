43181b8b80e31e1c167f888c3662633415516854
Andres <andreslemik@yandex.ru>
2015-07-27 23:30:57 +0300

    orders paranoid

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
+  acts_as_paranoid
   has_many :order_items
   has_many :products, through: :order_items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722122243) do
+ActiveRecord::Schema.define(version: 20150727203004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20150722122243) do
     t.string   "aasm_state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
   end
 
   create_table "product_option_types", force: :cascade do |t|
