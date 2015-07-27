1bd0d989a52bc58545d2945e0ca4045ca22d85d3
Andres <andreslemik@yandex.ru>
2015-07-27 23:29:19 +0300

    orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,15 @@
+# Order controller
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @orders = Order.all
+  end
+
   def show
+  end
+
+  def edit
   end
 
   def new
@@ -17,6 +25,23 @@ class OrdersController < ApplicationController
       else
         f.html { render :new }
       end
+    end
+  end
+
+  def update
+    respond_to do |f|
+      if @order.update(order_params)
+        f.html { redirect_to @order, notice: 'Договор успешно изменен' }
+      else
+        f.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @order.destroy
+    respond_to do |f|
+      f.html { redirect_to orders_url, notice: 'Договор удалён' }
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def ficon(icon, size=nil)
+    "<i class='fi-#{icon} #{size}'></i>".html_safe
+  end
 end
