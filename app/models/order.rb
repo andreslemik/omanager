9fa91ad327efb6402bdefba60d1826ab306d08b4
Andres <andreslemik@yandex.ru>
2015-07-28 00:37:44 +0300

    order with deleted authors and depts

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,19 @@
+# Order model class
 class Order < ActiveRecord::Base
   acts_as_paranoid
   has_paper_trail
   has_many :order_items
   has_many :products, through: :order_items
-  belongs_to :dept
-  belongs_to :author, class_name: User
+  belongs_to :dept, -> { with_deleted }
+  belongs_to :author, -> { with_deleted }, class_name: User
 
   validates :dept_id, presence: true
+
+  def author
+    User.unscoped { super }
+  end
+
+  def dept
+    Dept.unscoped { super }
+  end
 end
