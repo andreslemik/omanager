9cd5d1d3d26eb89d6f68c6f4f944c254d2123218
Andres <andreslemik@yandex.ru>
2015-08-06 14:38:37 +0300

    try stop migrating errors

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -1,35 +1,35 @@
-ActiveAdmin.register Role do
-  permit_params :name, :caption,
-                users_ids: []
-
-  filter :users
-  filter :caption
-
-
-  index do
-    selectable_column
-    id_column
-    column :caption
-    actions
-  end
-
-  form do |f|
-    f.inputs 'Роль пользователей' do
-      f.input :caption
-      f.input :name
-      end
-    f.actions
-  end
-
-  show do
-    panel 'Детали роли' do
-      attributes_table_for role do
-        row :id
-        row :caption
-        row :name
-        row :users_s
-      end
-    end
-  end
-
-end
+# ActiveAdmin.register Role do
+#   permit_params :name, :caption,
+#                 users_ids: []
+#
+#   filter :users
+#   filter :caption
+#
+#
+#   index do
+#     selectable_column
+#     id_column
+#     column :caption
+#     actions
+#   end
+#
+#   form do |f|
+#     f.inputs 'Роль пользователей' do
+#       f.input :caption
+#       f.input :name
+#       end
+#     f.actions
+#   end
+#
+#   show do
+#     panel 'Детали роли' do
+#       attributes_table_for role do
+#         row :id
+#         row :caption
+#         row :name
+#         row :users_s
+#       end
+#     end
+#   end
+#
+# end
