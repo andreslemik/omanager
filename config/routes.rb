9d3dd6c26ccdf64d359fd44591cde803686e2fc6
Andres <andreslemik@yandex.ru>
2015-08-06 14:52:15 +0300

    try stop migrating errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :orders
+
+
   devise_for :users#, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'home#index'
-
-  resources :orders
 end
