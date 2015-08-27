52a68d45af94c86566df77d898c5700c633e621f
Andres <andreslemik@yandex.ru>
2015-08-27 11:45:36 +0300

    configure logger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.logger = Logger.new("#{Rails.root}/log/development.log", 5, 5.megabytes)
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = Logger.new(config.paths.log.first, 5, 50.megabytes)
+  config.logger = Logger.new("#{Rails.root}/log/production.log", 3, 20.megabytes)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
