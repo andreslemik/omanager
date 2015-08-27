fc493c036393c29cf01a87187334941220819fec
Andres <andreslemik@yandex.ru>
2015-08-27 18:26:34 +0300

    tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,11 +2,31 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  def login_as(role)
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    user = FactoryGirl.create role
+    sign_in user
+  end
+
+  describe 'GET #index' do
+    context 'as unsigned user' do
+      it 'returns redircet to login' do
+        get :index
+        expect(response.code).to eq('302')
+      end
     end
+
+    context 'as signed user with any role' do
+      before :each do
+        role = [:admin, :manager, :factories].sample
+        login_as role
+      end
+      it 'return http succes' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+
   end
 
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,23 @@
+FactoryGirl.define do
+  Faker::Config.locale = :en
+  factory :user do
+    email { Faker::Internet.email }
+    pwd = Faker::Internet.password(8)
+    username { Faker::Internet.user_name }
+    password pwd
+    password_confirmation pwd
+    
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+
+    factory :admin do
+      after(:create) {|user| user.add_role :admin }
+    end
+    factory :manager do
+      after(:create) {|user| user.add_role :manager }
+    end
+    factory :fabrication do
+      after(:create) {|user| user.add_role :supplier }
+    end
+  end
+end
