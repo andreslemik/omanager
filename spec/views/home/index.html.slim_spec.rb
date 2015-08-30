22e05e0072ae33f3b014aaf816829ac8ba1ec700
Andres <andreslemik@yandex.ru>
2015-08-30 21:06:04 +0300

    index test

--- spec/views/home/index.html.slim_spec.rb
+++ spec/views/home/index.html.slim_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "home/index.html.slim", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'home/index.html.slim', type: :view do
+
+  it 'now contain nothing' do
+    render
+    expect(rendered).to match ''
+  end
+
 end
