be1e99b393c921e0d5d457bc711a21aa9ab99efe
Andres <andreslemik@yandex.ru>
2015-09-03 12:00:05 +0300

    order edit form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def ficon(name, size = nil)
-    content_tag(:i, nil, class: "fi-#{name} #{size}")
+    content_tag(:i, nil, class: "fi-#{name} #{size} icon")
   end
 end
