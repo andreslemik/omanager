1bd9e3cf59d8bef909272fb74cfd446ac51d9ae0
Andres <andreslemik@yandex.ru>
2015-09-12 22:12:57 +0300

    change order_items migration

--- db/migrate/20150912172751_add_aas_mto_order_items.rb
+++ db/migrate/20150912172751_add_aas_mto_order_items.rb
@@ -2,5 +2,9 @@ class AddAasMtoOrderItems < ActiveRecord::Migration
   def change
     add_column :order_items, :aasm_state, :string
     add_index :order_items, :aasm_state
+
+    execute <<-SQL
+      UPDATE order_items SET aasm_state = 'pending';
+    SQL
   end
 end
