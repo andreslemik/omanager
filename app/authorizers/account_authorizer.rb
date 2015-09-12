4a02b7dfb85aaca53e97cca605bf1b9cafc7e936
Andres <andreslemik@yandex.ru>
2015-09-12 19:39:14 +0300

    abilities for accounts

--- app/authorizers/account_authorizer.rb
+++ app/authorizers/account_authorizer.rb
@@ -12,12 +12,14 @@ class AccountAuthorizer < ApplicationAuthorizer
     def creatable_by?(user)
       user.has_any_role? :admin, :accountant
     end
-
   end
 
   def updatable_by?(user)
-    return true if user.has_role? :admin
+    if resource.accountable_type == 'Partner'
+      # Нельзя редактировать расход, ссылающийся на договор,для операций контрагентов
+      return false if !resource.order_id.nil? && resource.operation_type == 'expense'
+    end
+    return true if user.has_role?(:admin)
     Time.now - resource.created_at < 30.days && user.has_role?(:accountant)
   end
-
 end
