16136eff9cdf417bdd6c11720ba86384132a5de6
Andres <andreslemik@yandex.ru>
2015-09-17 15:34:45 +0300

    small fix

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,7 +1,7 @@
 module OrdersHelper
   def order_items_text(order)
     result = ''
-    order.order_items.each do |oi|
+    order.order_items.to_fabrication.each do |oi|
       result << content_tag(:span, oi.product.name, title: oi.product.category.name)
       result << '<br />' unless oi == order.order_items.last
     end
