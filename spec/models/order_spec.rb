572cec03b60a8bbc66213906f4237aa465fb09cb
Andres <andreslemik@yandex.ru>
2015-09-22 20:24:57 +0300

    fix test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'create one order' do
+    expect {
+      create :order
+    }.to change(Order, :count).by 1
+  end
+
+  it 'order created with 3 order_items' do
+    expect {
+      create :order
+    }.to change(OrderItem, :count).by 3
+  end
 end
