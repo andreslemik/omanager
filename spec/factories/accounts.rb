819b46da85f07d5ea0a55a6e77be2bce4fc5cf8f
Andres <andreslemik@yandex.ru>
2015-09-22 20:20:40 +0300

    fix test

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :account do
     amount { Faker::Number.number 4 }
     operation_date { Faker::Date.between 6.months.ago, Date.today }
-    #TODO add other associations when they will appear
+
     trait :partner_operation do
       association :accountable, factory: [:partner, :customer]
     end
