8b0ee2262909d848acb736e2669bdb87c69596c0
Andres <andreslemik@yandex.ru>
2015-09-22 20:01:46 +0300

    fix test

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe OrderItemsController, type: :controller do
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
+  def login_as(role)
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    user = FactoryGirl.create role
+    sign_in user
+  end
+  before :each do
+    @order = FactoryGirl.create :retail_order
+    login_as :admin
+  end
+  describe 'GET #new' do
+    it 'returns http success' do
+      get :new, { order_id: @order.id }
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
+  describe 'GET #edit' do
+    it 'returns http success' do
+      get :edit, { id: @order.order_items.first }
       expect(response).to have_http_status(:success)
     end
   end
