6dc36e8bca32f6eef9221a169768b5c80d966abe
Andres <andreslemik@yandex.ru>
2015-09-22 21:23:51 +0300

    order_item model test

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'one order_item state changes' do
+    before do
+      @order_item = FactoryGirl.create :order_item
+      @order_item.fabrication_date = Date.today
+      @order_item.save!
+    end
+    it 'change fabrication_date leads to order_item state change' do
+      expect(@order_item.aasm_state).to eq('working')
+    end
+    it 'change fabrication_date leads to order change state to working' do
+      expect(@order_item.order.aasm_state).to eq('working')
+    end
+    it 'change order_item state to pending will change order state to pending' do
+      @order_item.stop_work!
+      expect(@order_item.order.aasm_state).to eq('pending')
+    end
+  end
+
+  context 'three order_items states change' do
+    before do
+      @order = FactoryGirl.create :order
+    end
+    it 'one item change state to working change order state too' do
+      order_item = @order.order_items.first
+      order_item.fabrication_date = Date.today
+      order_item.save!
+      expect(@order.aasm_state).to eq('working')
+    end
+    it 'after one of two items state change back order not change' do
+      order_item = nil
+      (0..1).each do |i|
+        order_item = @order.order_items[i]
+        order_item.fabrication_date = Date.today
+        order_item.save!
+      end
+      expect{order_item.stop_work!}.to raise_error(AASM::InvalidTransition)
+    end
+  end
 end
