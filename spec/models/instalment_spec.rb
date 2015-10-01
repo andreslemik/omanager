ff57e7dd24665173b8aee676d17997ff8804e4a2
Andres <andreslemik@yandex.ru>
2015-10-01 16:20:39 +0300

    instalment model spec

--- spec/models/instalment_spec.rb
+++ spec/models/instalment_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Instalment, type: :model do
   it 'create 6 instalments' do
-    expect {
+    expect do
       create_list :instalment, 6
-    }.to change(Instalment, :count).by 6
+    end.to change(Instalment, :count).by 6
+  end
+
+  it 'two instalments by 5000 summary' do
+    create_list :instalment, 2, amount: 5000
+    expect(Instalment.summary).to eq(10_000)
+  end
+
+  it 'two of three instalments after 01.01.2016' do
+    (0..2).each do |i|
+      FactoryGirl.create :instalment,
+                         payment_date: Date.parse('05.12.2015') + i.months
+    end
+    expect(Instalment.after_date('01.01.2016').count).to eq(2)
   end
 end
