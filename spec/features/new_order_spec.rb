fa24bc29e1051dda3ee261276e79675cfc0f86fd
Andres <andreslemik@yandex.ru>
2015-10-03 16:17:43 +0300

    tests

--- spec/features/new_order_spec.rb
+++ spec/features/new_order_spec.rb
@@ -5,11 +5,18 @@ describe 'New order feature', type: :feature do
     user = FactoryGirl.create :manager
     login_as user
   end
-  it 'open new order form', js: true do
+  it 'choose dealer type', js: true do
     visit new_order_path
     within('#new_order') do
       choose 'order_order_type_dealer'
     end
     expect(page).to have_selector('.corporate', visible: true)
+  end
+  it 'choose internal order type', js: true do
+    visit new_order_path
+    within('#new_order') do
+      choose 'order_order_type_internal'
+    end
+    expect(page).to have_selector('#client_definition', visible: false)
   end
 end
