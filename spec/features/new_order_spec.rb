700db3e66c6efb994713faee069df47309585ac4
Andres <andreslemik@yandex.ru>
2015-10-03 19:20:56 +0300

    test

--- spec/features/new_order_spec.rb
+++ spec/features/new_order_spec.rb
@@ -4,6 +4,7 @@ describe 'New order feature', type: :feature do
   before :each do
     user = FactoryGirl.create :manager
     login_as user
+    FactoryGirl.create_list :product, 5
   end
   context 'Choosing order type' do
     it 'choose dealer type', js: true do
@@ -19,6 +20,24 @@ describe 'New order feature', type: :feature do
         choose 'order_order_type_internal'
       end
       expect(page).to have_selector('#client_definition', visible: false)
+    end
+  end
+  context 'Creating order' do
+    it 'create retail order', js: true, driver: :selenium do
+      visit new_order_path
+      product = Product.all.sample
+      within '#new_order' do
+        fill_in 'order_client', with: 'Иванов Иван Иванович'
+        fill_in 'order_phone', with: '123-54-23'
+        fill_in 'order_address', with: 'Ленина, 1'
+        select 'Центр', from: 'order_area'
+
+        select product.category.name, from: 'order_order_items_attributes_0_category'
+        select product.manufacturer.name, from: 'order_order_items_attributes_0_manufacturer'
+        select product.name, from: 'order_order_items_attributes_0_product_id'
+      end
+      expect(find_field('order_order_items_attributes_0_cost').value).to eq(product.price.to_s)
+      #sleep(inspection_time=5)
     end
   end
 end
