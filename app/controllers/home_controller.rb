d2447e08ae40e9a32d9f6fad211af4c361f44317
Andres <andreslemik@yandex.ru>
2015-11-05 10:57:10 +0300

    add navigation system

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  PAGES = %w(orders accounting production delivery store)
   def index
+  end
+
+  def nav
+    page = PAGES.select { |p| p == params[:page] }.first
+    raise ActionController::RoutingError.new('Page Not Found') if page.blank?
+    render "home/#{page}"
   end
 end
