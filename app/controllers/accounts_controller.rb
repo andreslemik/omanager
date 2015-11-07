18c1b79ec3232c92ff434ed275c52b0370db07fc
Andres <andreslemik@yandex.ru>
2015-11-08 02:17:08 +0300

    fix account controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -81,19 +81,22 @@ class AccountsController < ApplicationController
   def find_accounter
     allowed_types = %w(order partner)
     fail 'Not allowed parameter' unless allowed_types.include? params[:accounter_type]
-    accounter_type = params[:accounter_type]
-    klass = accounter_type.capitalize.constantize
+    klass = allowed_models.find { |c| c.name == params[:accounter_type].classify }
     @accounter = klass.find(params[:accounter_id])
   end
 
   def set_account
     @account = Account.find params[:id]
-    klass = @account.accountable_type.capitalize.constantize
+    klass = allowed_models.finf { |c| c.name == @account.accountable_type}
     @accounter = klass.find(@account.accountable_id)
   end
 
   def account_params
     params.require(:account)
       .permit(:operation_type, :operation_date, :amount, :memo, :dept_id)
+  end
+
+  def allowed_models
+    [Order, Partner]
   end
 end
