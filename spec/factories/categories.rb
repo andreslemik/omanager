675ceb7f0b96d69127625a6479788f25805af81e
Andres <andreslemik@yandex.ru>
2015-11-08 01:29:07 +0300

    small fix (cherry picked from commit 8881e20)

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :category do
-    name { "#{Faker::Name.name} #{rand 1_000_000}" }
+    name { "#{Faker::Lorem.word} #{rand 1_000_000}" }
     fabrication true
   end
 end
